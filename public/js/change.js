52d354e29a8b099b758d14863495b48e8d74b061
lxzanaa <zari.weone2010@gmail.com>
2024-12-30 19:44:27 +0300

    add

--- public/js/change.js
+++ public/js/change.js
@@ -1,39 +1,64 @@
-const buttons = document.querySelectorAll('.change_btn');
-const sections = document.querySelectorAll('.change__section');
+const buttons = document.querySelectorAll('.change_btn')
+const sections = document.querySelectorAll('.change__section')
 
 buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
+  button.addEventListener('click', () => {
+    buttons.forEach(btn => btn.classList.remove('active'))
+    button.classList.add('active')
 
-        sections.forEach(section => section.classList.remove('active'));
-        sections[index].classList.add('active');
-    });
-});
+    sections.forEach(section => section.classList.remove('active'))
+    sections[index].classList.add('active')
+  })
+})
 
-const initialActiveButton = document.querySelector('.change_btn.active');
+const initialActiveButton = document.querySelector('.change_btn.active')
 if (initialActiveButton) {
-    const initialSectionIndex = initialActiveButton.getAttribute('data-section');
-    sections.forEach((section, index) => {
-        section.classList.toggle('active', index == initialSectionIndex);
-    });
+  const initialSectionIndex = initialActiveButton.getAttribute('data-section')
+  sections.forEach((section, index) => {
+    section.classList.toggle('active', index == initialSectionIndex)
+  })
 }
+
+const buttons_change = document.querySelectorAll('.change_sertificate')
+const sections_change = document.querySelectorAll('.change__sec-sertificate')
+
+buttons_change.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    buttons_change.forEach(btn => btn.classList.remove('active'))
+    button.classList.add('active')
+
+    sections_change.forEach(section => section.classList.remove('active'))
+    sections_change[index].classList.add('active')
+  })
+})
+
+const initialActiveButton_change = document.querySelector(
+  '.change_sertificate.active'
+)
+if (initialActiveButton_change) {
+  const initialSectionIndex =
+    initialActiveButton_change.getAttribute('data-section')
+  sections_change.forEach((section, index) => {
+    section.classList.toggle('active', index == initialSectionIndex)
+  })
+}
+
 document.querySelectorAll('.checkbox').forEach(function (checkbox) {
-    checkbox.addEventListener('change', function () {
-        const index = this.id.replace('checkbox', '');
+  checkbox.addEventListener('change', function () {
+    const index = this.id.replace('checkbox', '')
 
-        const defaultSVG = document.getElementById('default-svg' + index);
-        const checkmarkSVG = document.getElementById('checkmark-svg' + index);
-        const container = document.getElementById('checkbox-container' + index);
+    const defaultSVG = document.getElementById('default-svg' + index)
+    const checkmarkSVG = document.getElementById('checkmark-svg' + index)
+    const container = document.getElementById('checkbox-container' + index)
 
-        if (this.checked) {
-            defaultSVG.classList.add('hidden');
-            checkmarkSVG.classList.remove('hidden');
-            container.classList.add('bg-[#4852F5]', 'border-[#FFFFFF4D]');
-        } else {
-            defaultSVG.classList.remove('hidden');
-            checkmarkSVG.classList.add('hidden');
-            container.classList.remove('bg-[#4852F5]', 'border-[#FFFFFF4D]');
-        }
-    });
-});
+    if (this.checked) {
+      defaultSVG.classList.add('hidden')
+      checkmarkSVG.classList.remove('hidden')
+      container.classList.add('bg-[#4852F5]', 'border-[#FFFFFF4D]')
+    } else {
+      defaultSVG.classList.remove('hidden')
+      checkmarkSVG.classList.add('hidden')
+      container.classList.remove('bg-[#4852F5]', 'border-[#FFFFFF4D]')
+    }
+  })
+})
